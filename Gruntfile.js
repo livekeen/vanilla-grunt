e8bd169f9cadf1f3a48ba0ed3ed9309be785b801
Kevin Lee <kevin@livekeen.com>
2013-12-28 16:59:00 -0500

    Added sass + watch

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,14 +13,52 @@ module.exports = function(grunt) {
         src: 'src/<%= pkg.name %>.js',
         dest: 'build/<%= pkg.name %>.min.js'
       }
+    },
+
+    sass: {                              // Task
+      dist: {                            // Target
+        options: {                       // Target options
+          style: 'compressed'
+        },
+        files: {                         // Dictionary of files
+          'build/assets/css/styles.css': 'app/assets/sass/styles.sass'    // 'destination': 'source'
+        }
+      }
+    },
+
+    imagemin: {
+      dynamic: {
+          files: [{
+              expand: true,
+              cwd: 'app/assets/img/',
+              src: ['**/*.{png,jpg,gif}'],
+              dest: 'build/assets/img/'
+          }]
+      }
+    },
+
+    watch: {
+      options: {
+        livereload: true,
+      },
+      scripts: {
+        files: ['js/*.js'],
+        tasks: ['uglify'],
+        options: {
+          spawn: false,
+        },
+      } 
     }
     
   });
 
-  // Load the plugin that provides the "uglify" task.
+  // Load the plugin(s).
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
   // Default task(s).
-  grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('default', ['uglify', 'sass', 'imagemin']);
 
 };
---
+++ README.md
@@ -0,0 +1,5 @@
+# Getting Started
+
+- Change to this project's root directory
+- Install project dependencie with `npm install`
+- Run Grunt with `grunt`
